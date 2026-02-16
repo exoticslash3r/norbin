9142ceed7a0dc905f235c73cc929f4aade6208be
exoticslash3r <exoticslash3r@gmail.com>
2026-02-16 18:02:19 +0000

    Add Firebase configuration to config.js
    Added Firebase configuration settings for main and chat databases.

---
+++ config.js
@@ -0,0 +1,18 @@
+// Firebase Configuration - ADD THIS FILE TO .GITIGNORE
+window.FIREBASE_CONFIG = {
+    // Main database
+    apiKey: "AIzaSyA1qaJnsRvWDvxNn9Nd1tmMVjvkF-WpUQY",
+    authDomain: "norbin-c2fdb.firebaseapp.com",
+    projectId: "norbin-c2fdb",
+    storageBucket: "norbin-c2fdb.firebasestorage.app",
+    messagingSenderId: "38678324468",
+    appId: "1:38678324468:web:99301da909b4a563312aa1",
+    
+    // Chat database
+    chatApiKey: "AIzaSyByN2D1Q9IasxBI9qNostCzg13WbIFipUc",
+    chatAuthDomain: "norbin-chat.firebaseapp.com",
+    chatProjectId: "norbin-chat",
+    chatStorageBucket: "norbin-chat.firebasestorage.app",
+    chatMessagingSenderId: "793931248044",
+    chatAppId: "1:793931248044:web:80a954c39e17efafd58a09"
+};
